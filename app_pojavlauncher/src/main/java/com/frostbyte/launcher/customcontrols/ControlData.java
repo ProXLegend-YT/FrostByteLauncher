25b847fb242065a3574ed486137ca189ba682c46
ProXLegend-YT <ytproxlegend@gmail.com>
2026-08-30 21:49:03 +0500

    Fix invisible loading spinner and round the real default-button source

--- app_pojavlauncher/src/main/java/com/frostbyte/launcher/customcontrols/ControlData.java
+++ app_pojavlauncher/src/main/java/com/frostbyte/launcher/customcontrols/ControlData.java
@@ -108,7 +108,7 @@ public class ControlData {
     }
 
     public ControlData(String name, int[] keycodes, String dynamicX, String dynamicY, float width, float height, boolean isToggle) {
-        this(name, keycodes, dynamicX, dynamicY, width, height, isToggle, 1, 0x4D000000, 0xFFFFFFFF, 0, 0, true, true, false, false, null);
+        this(name, keycodes, dynamicX, dynamicY, width, height, isToggle, 1, 0x4D000000, 0xFFFFFFFF, 0, 28, true, true, false, false, null);
     }
 
     public ControlData(String name, int[] keycodes, String dynamicX, String dynamicY, float width, float height, boolean isToggle, float opacity, int bgColor, int strokeColor, float strokeWidth, float cornerRadius, boolean displayInGame, boolean displayInMenu, boolean isSwipable, boolean mousePassthrough, String bitmapTag) {
